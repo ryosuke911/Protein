b0ae6cdfdf342b02d9139956389aa57c0456164a
ryosuke911 <ryosuke3028911@gmail.com>
2023-03-14 02:25:22 +0900

    paging,devise for Japanese

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
  before_action :authenticate_account!, except: [:index]
  before_action :ensure_account, only: [:show, :edit, :delete]
   def index
-    @posts= Post.all
+    @posts= Post.all.page(params[:page]).per(10)
   end
 
   def show
